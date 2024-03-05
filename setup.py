fe1ad9871e02758e6ffbdc94402da1f6dbf0cd7c
Riley Brady <riley_brady@mckinsey.com>
2024-03-05 14:55:47 -0700

    edit setup

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('requirements.txt') as f:
-    install_requires = f.read().strip().split('\n')
-
 if __name__ == '__main__':
     setup(
         maintainer='Luke Davis',
         maintainer_email='lukelbd@gmail.com',
         description='A succinct matplotlib wrapper for making beautiful graphics.',
-        install_requires=install_requires,
+        install_requires=['matplotlib>=3.0.0', 'numpy'],
         python_requires='>=3.6',
         packages=find_packages(),
         version='1.0.1',
