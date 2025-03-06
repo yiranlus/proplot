30dc386b8a8c59dbff1ba9b6543fa0cf665391af
Yiran Lu <yiranls@outlook.com>
2025-03-06 13:59:52 +0100

    shared axis API change (compatible with matplotlib > 3.6)

--- proplot/axes/cartesian.py
+++ proplot/axes/cartesian.py
@@ -558,7 +558,12 @@ class CartesianAxes(shared._SharedAxes, plot.PlotAxes):
             if ax1.get_autoscalex_on() and not ax2.get_autoscalex_on():
                 ax1.set_xlim(ax2.get_xlim())  # non-default limits
         # Copy non-default locators and formatters
-        self.get_shared_x_axes().join(self, sharex)  # share limit/scale changes
+        # share limit/scale changes
+        try:
+            # matplotlib <= 3.6
+            self.get_shared_x_axes().join(self, sharex)
+        except AttributeError:
+            self.sharex(sharex)
         if sharex.xaxis.isDefault_majloc and not self.xaxis.isDefault_majloc:
             sharex.xaxis.set_major_locator(self.xaxis.get_major_locator())
         if sharex.xaxis.isDefault_minloc and not self.xaxis.isDefault_minloc:
@@ -580,7 +585,12 @@ class CartesianAxes(shared._SharedAxes, plot.PlotAxes):
                 ax1.set_yscale(ax2.get_yscale())
             if ax1.get_autoscaley_on() and not ax2.get_autoscaley_on():
                 ax1.set_ylim(ax2.get_ylim())
-        self.get_shared_y_axes().join(self, sharey)  # share limit/scale changes
+        # share limit/scale changes
+        try:
+            # matplotlib <= 3.6
+            self.get_shared_y_axes().join(self, sharey)
+        except AttributeError:
+            self.sharey(sharey)
         if sharey.yaxis.isDefault_majloc and not self.yaxis.isDefault_majloc:
             sharey.yaxis.set_major_locator(self.yaxis.get_major_locator())
         if sharey.yaxis.isDefault_minloc and not self.yaxis.isDefault_minloc:
